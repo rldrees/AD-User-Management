99a2c02e6dc913fd2fa134c4df973e0e1f78a39a
rldrees <R.Drees@powerdrives.com>
2016-11-23 06:50:32 -0500

    Changed file location in Zip.ps1

--- Zip.ps1
+++ Zip.ps1
@@ -2,13 +2,13 @@ $DomainUserName = New-Object System.security.principal.NTAccount("powerdrives.lo
 $DenyControl = New-Object  system.security.accesscontrol.filesystemaccessrule($DomainUserName,"FullControl","ContainerInherit,ObjectInherit", "None","Deny")
 
 #Zipping up old user files
-write-zip ("\\VMUSERDATA1\f$\UserData\" + $Location + "\" + $UserName) ("\\VMUSERDATA1\f$\UserData\" + $Location + "\" + $UserName + ".zip")
+write-zip ("\\C3P0.powerdrives.local\g$\User_Data_Files" + $Location + "\" + $UserName) ("\\C3P0.powerdrives.local\g$\User_Data_Files" + $Location + "\" + $UserName + ".zip")
 
 #Moving Files from VMUserdata to NAS Data
-Move-Item ("\\VMUSERDATA1\f$\UserData\" + $Location + "\" + $UserName + ".zip") "\\C3P0.powerdrives.local\e$\Termed User Folders\pst waiting area\"
+Move-Item ("\\C3P0.powerdrives.local\g$\User_Data_Files" + $Location + "\" + $UserName + ".zip") "\\C3P0.powerdrives.local\e$\Termed User Folders\pst waiting area\"
 
 #Remove old file location from VMUserdata
-$ACL = Get-ACL ("\\VMUSERDATA1\f$\UserData\" + $Location + "\" + $UserName)
+$ACL = Get-ACL ("\\C3P0.powerdrives.local\g$\User_Data_Files" + $Location + "\" + $UserName)
 $ACL.SetAccessRule($DenyControl)
-set-acl ("\\VMUSERDATA1\f$\UserData\" + $Location + "\" + $UserName) $ACL
-Remove-Item ("\\VMUSERDATA1\f$\UserData\" + $Location + "\" + $UserName) -Recurse -Force
+set-acl ("\\C3P0.powerdrives.local\g$\User_Data_Files" + $Location + "\" + $UserName) $ACL
+Remove-Item ("\\C3P0.powerdrives.local\g$\User_Data_Files" + $Location + "\" + $UserName) -Recurse -Force
